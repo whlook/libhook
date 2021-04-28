b28c42219f765e3f1e8a40c8af7d90355f449bc0
wuhui <wuhui@sensetime.com>
2021-04-28 11:08:20 +0800

    fix lib static

--- hook.c
+++ hook.c
@@ -13,65 +13,71 @@ typedef struct _hk_mem_stat_info{
 	unsigned long long int unmapped_; // TODO
 } _hk_mem_stat_info_t;
 
-static _hk_mem_stat_info_t _hk_info_;
-static unsigned char _hk_start_ = 0;
-static std::map<void*,unsigned long long int> _hk_alloc_rec_;
-
-void _hk_start_mem_stat()
+class _HK_STAT_
 {
-	_hk_start_ = 1;
-	_hk_info_.alloced_ = 0;
-	_hk_info_.released_ = 0;
-	_hk_info_.mapped_ = 0;
-	_hk_info_.unmapped_ = 0;
-	_hk_alloc_rec_.clear();
-}
 
-void _hk_stop_mem_stat(const char * save_file)
-{
-	(void)save_file;
-	_hk_start_ = 0;
-	printf("alloced:%lld\n",_hk_info_.alloced_);
-	printf("released:%lld\n",_hk_info_.released_);
-	printf("mapped:%lld\n",_hk_info_.mapped_);
-	printf("unmapped:%lld\n",_hk_info_.unmapped_);
-}
+public:
+	_HK_STAT_()
+	{
+		_hk_info_.alloced_ = 0;
+		_hk_info_.released_ = 0;
+		_hk_info_.mapped_ = 0;
+		_hk_info_.unmapped_ = 0;
+		_hk_alloc_rec_.clear();
+	}
 
-void _hk_mem_alloced_inc(void* va, size_t size)
-{
-	if(unlikely(_hk_start_ == 0)) return;	
-	if(unlikely(va == NULL)) return;	
-	_hk_info_.alloced_ += size;
-	_hk_alloc_rec_[va] = size;
-}
+	~_HK_STAT_()
+	{
+		printf("alloced:%lld\n",_hk_info_.alloced_);
+		printf("released:%lld\n",_hk_info_.released_);
+		printf("mapped:%lld\n",_hk_info_.mapped_);
+		printf("unmapped:%lld\n",_hk_info_.unmapped_);
+	}
 
-void _hk_mem_released_inc(void* va)
-{
-	if(unlikely(_hk_start_ == 0)) return;
-	if(unlikely(va == NULL)) return;	
-	_hk_info_.released_ += _hk_alloc_rec_[va];
-	_hk_alloc_rec_[va] = 0;
-}
+	void _hk_mem_alloced_inc(void* va, size_t size)
+	{
+		if(unlikely(va == NULL)) return;	
+		_hk_info_.alloced_ += size;
+		_hk_alloc_rec_[va] = size;
+	}
 
+	void _hk_mem_released_inc(void* va)
+	{
+		if(unlikely(va == NULL)) return;	
+		_hk_info_.released_ += _hk_alloc_rec_[va];
+		_hk_alloc_rec_[va] = 0;
+	}
+
+private:
+	_hk_mem_stat_info_t _hk_info_;
+	std::map<void*,unsigned long long int> _hk_alloc_rec_;
+};
+
+static _HK_STAT_ _hk_stat_;
 //
 // ------------------------ hooks ------------------------ //
+// dont hook system malloc/free
 //
-typedef void* (*some_malloc_fn)(size_t);
-void * someMalloc(size_t size)
+extern "C" {
+
+typedef unsigned char * (*some_malloc_fn)(size_t);
+unsigned char* some_malloc(size_t size)
 {
 	some_malloc_fn raw;
-	raw=(some_malloc_fn)dlsym(RTLD_NEXT,"someMalloc");
-	void *va = raw(size);
-	_hk_mem_alloced_inc(va,size);
+	raw=(some_malloc_fn)dlsym(RTLD_NEXT,"some_malloc");
+	unsigned char*va = (unsigned char*)raw(size);
+	_hk_stat_._hk_mem_alloced_inc((void*)va,size);
 	return va;
 }
 
-typedef void* (*some_free_fn)(void*);
-void * someFree(void* va)
+typedef int (*some_free_fn)(unsigned char*);
+int some_free(unsigned char* va)
 {
 	some_free_fn raw;
-	raw=(some_free_fn)dlsym(RTLD_NEXT,"someFree");
-	void *ret = raw(va);
-	_hk_mem_released_inc(va);
+	raw=(some_free_fn)dlsym(RTLD_NEXT,"some_free");
+	int ret = raw(va);
+	_hk_stat_._hk_mem_released_inc((void*)va);
 	return ret;
+}
+
 }
